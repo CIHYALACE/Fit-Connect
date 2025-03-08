c8f38e17e5025b1995ed0d0af08dcdc038910b17
AbdelRahmna <abodyoussef2015@gmail.com>
2025-03-09 01:25:57 +0200

    simple edit in cards

--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -6,29 +6,29 @@ export default function ProductsSection() {
         <div className="d-flex justify-content-between flex-wrap h-75 text-center">
 
           <div className="col-6 col-md-3 px-1 px-md-4">
-            <img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" />
-            <h4 className="big-shoulders fw-bold">Program Name</h4>
+            <a href="#"><img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" /></a>
+            <a href="#" className="text-decoration-none "><h4 className="big-shoulders fw-bold text-dark ">Program Name</h4></a>
             <p>Author:Author Name</p>
             <p>Price:Program Price</p>
           </div>
 
           <div className="col-6 col-md-3 px-1 px-md-4">
-            <img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" />
-            <h4 className="big-shoulders fw-bold">Program Name</h4>
+            <a href="#"><img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" /></a>
+            <a href="#" className="text-decoration-none "><h4 className="big-shoulders fw-bold text-dark ">Program Name</h4></a>
             <p>Author:Author Name</p>
             <p>Price:Program Price</p>
           </div>
 
           <div className="col-6 col-md-3 px-1 px-md-4">
-            <img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" />
-            <h4 className="big-shoulders fw-bold">Program Name</h4>
+            <a href="#"><img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" /></a>
+            <a href="#" className="text-decoration-none "><h4 className="big-shoulders fw-bold text-dark ">Program Name</h4></a>
             <p>Author:Author Name</p>
             <p>Price:Program Price</p>
           </div>
 
           <div className="col-6 col-md-3 px-1 px-md-4">
-            <img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" />
-            <h4 className="big-shoulders fw-bold">Program Name</h4>
+            <a href="#"><img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" /></a>
+            <a href="#" className="text-decoration-none "><h4 className="big-shoulders fw-bold text-dark ">Program Name</h4></a>
             <p>Author:Author Name</p>
             <p>Price:Program Price</p>
           </div>
