ecfed6d89dc5eaaec9a2986d74b4305deade24af
AbdelRahmna <abodyoussef2015@gmail.com>
2025-03-09 01:20:11 +0200

    improve Product Section

--- src/components/ProductsSection.jsx
+++ src/components/ProductsSection.jsx
@@ -1,44 +1,44 @@
 export default function ProductsSection() {
   return (
     <>
-      <div className="vh-100 container">
-        <h3>Trainig Programs:</h3>
+      <div className="vh-100 container mt-5">
+        <h3 className="big-shoulders fw-bold mb-4">Trainig Programs:</h3>
         <div className="d-flex justify-content-between flex-wrap h-75 text-center">
 
-          <div className="col-6 col-md-2 px-1 px-md-0">
+          <div className="col-6 col-md-3 px-1 px-md-4">
             <img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" />
-            <h4>Program Name</h4>
+            <h4 className="big-shoulders fw-bold">Program Name</h4>
             <p>Author:Author Name</p>
             <p>Price:Program Price</p>
           </div>
 
-          <div className="col-6 col-md-2 px-1 px-md-0">
+          <div className="col-6 col-md-3 px-1 px-md-4">
             <img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" />
-            <h4>Program Name</h4>
+            <h4 className="big-shoulders fw-bold">Program Name</h4>
             <p>Author:Author Name</p>
             <p>Price:Program Price</p>
           </div>
 
-          <div className="col-6 col-md-2 px-1 px-md-0">
+          <div className="col-6 col-md-3 px-1 px-md-4">
             <img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" />
-            <h4>Program Name</h4>
+            <h4 className="big-shoulders fw-bold">Program Name</h4>
             <p>Author:Author Name</p>
             <p>Price:Program Price</p>
           </div>
 
-          <div className="col-6 col-md-2 px-1 px-md-0">
+          <div className="col-6 col-md-3 px-1 px-md-4">
             <img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" />
-            <h4>Program Name</h4>
+            <h4 className="big-shoulders fw-bold">Program Name</h4>
             <p>Author:Author Name</p>
             <p>Price:Program Price</p>
           </div>
 
-          <div className="col-6 col-md-2 d-none d-md-block">
+          {/* <div className="col-6 col-md-3 d-none d-md-block">
             <img src="../../public/pexels-ivan-samkov-4162459.jpg" width={"100%"} alt="" />
-            <h4>Program Name</h4>
+            <h4 className="big-shoulders fw-bold">Program Name</h4>
             <p>Author:Author Name</p>
             <p>Price:Program Price</p>
-          </div>
+          </div> */}
 
         </div>
       </div>
