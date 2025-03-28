8c6479e8c557023ee33cd3000122acc6017d6ff9
AbdelRahmna <abodyoussef2015@gmail.com>
2025-03-28 02:41:09 +0200

    Finish Third Section

---
+++ frontend/src/components/CouchesSection.jsx
@@ -0,0 +1,15 @@
+export default function CouchesSection() {
+    return (
+        <>
+            <div className="responsive-height-2 responsive-height-portrait container rounded bg-dark d-flex justify-content-center align-items-center flex-wrap overflow-hidden">
+                <div className="col-12 col-md-6 text-box mt-5 mt-md-0">
+                    <h1 className="text-white mb-4">Couches</h1>
+                    <p className="text-white mb-2">Upgrade your home with our luxurious, high-quality couches. Perfect comfort, timeless design—because you deserve the best.</p>
+                    <button className="btn btn-light">View Couches</button>
+                </div>
+                <img src="../../public/BigMan.jpeg" alt="Couches" className=" align-self-end align-self-md-start col-12 col-md-3 position-relative" />
+
+            </div>
+        </>
+    )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import HeroSection from "../components/HeroSection";
 import ProductsSection from "../components/ProductsSection";
+import CouchesSection from "../components/CouchesSection";
 
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
     <>
       <HeroSection />
       <ProductsSection />
+      <CouchesSection />
     </>
   );
 }
