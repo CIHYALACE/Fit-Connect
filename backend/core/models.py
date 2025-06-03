938908a5da000796209ae45c0ef1152171e06328
AbdelRahman <abodyoussef2015@gmail.com>
2025-06-04 00:32:02 +0300

    Refactor TrainingPrograms model: correct field names and add difficulty level choices

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from users.models import CouchProfile
 
+
+DIFFICULTY_COICES = [
+    ("begginer","Beginner"),
+    ("intermediate", "Intermediate"),
+    ("advanced", "Advanced")
+]
 class TrainingPrograms(models.Model):
-    name = models.CharField(max_length=100)
-    # auther = models.ForeignKey(to=Couches)
+    title = models.CharField(max_length=100)
+    auther = models.ForeignKey(CouchProfile, on_delete=models.CASCADE)
     duration = models.CharField()
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    difficulty_level = models.CharField(choices=DIFFICULTY_COICES)
+    image = models.ImageField(upload_to=None)
