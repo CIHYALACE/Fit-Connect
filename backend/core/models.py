31ffc4a68b2489274b8743fcec9565af375c332e
AbdelRahman <abodyoussef2015@gmail.com>
2025-06-03 23:39:18 +0300

    Refactor TrainingPrograms model: update fields and remove Client model definition

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
-
-class Client(models.Model):
-    name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
+class TrainingPrograms(models.Model):
+    name = models.CharField(max_length=100)
+    # auther = models.ForeignKey(to=Couches)
+    duration = models.CharField()
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
