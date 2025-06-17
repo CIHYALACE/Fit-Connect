97866cffe21a8652ef85cc78ef4f9cddda30a449
AbdelRahman <abodyoussef2015@gmail.com>
2025-06-17 23:11:26 +0300

    Refactor code structure for improved readability and maintainability

--- backend/fitConnect/settings.py
+++ backend/fitConnect/settings.py
@@ -95,12 +95,8 @@ WSGI_APPLICATION = 'fitConnect.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Changed from django_tenants.postgresql_backend
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'Abod@2000',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
