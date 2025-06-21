e08014845a9a4ebcee7cfc2d26867eb41f61912e
AbdelRahman <abodyoussef2015@gmail.com>
2025-06-21 16:11:06 +0300

    Add media URL handling in development and fix typo in router registration for CoachProfile

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import CoachProfile
 
 class CoachProfileSerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source='user.email', read_only=True)
     class Meta:
         model = CoachProfile
         fields = '__all__'
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,8 +3,9 @@ from rest_framework.routers import DefaultRouter
 from .views import CoachProfileViewSet
 
 router = DefaultRouter()
-router.register(r'couches_list', CoachProfileViewSet)
+router.register(r'coaches_list', CoachProfileViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('coaches_list/<int:pk>', CoachProfileViewSet.as_view({'get':'retrieve'}), name='coach_profile_detail')
 ]
