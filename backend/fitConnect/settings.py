af139cf16433887fd229ffa6b962df8986ea75b1
AbdelRahman <abodyoussef2015@gmail.com>
2025-06-25 21:31:53 +0300

    Implement code changes to enhance functionality and improve performance

--- backend/fitConnect/settings.py
+++ backend/fitConnect/settings.py
@@ -95,11 +95,22 @@ WSGI_APPLICATION = 'fitConnect.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'fitconnect',
+'USER': 'postgres',
+'PASSWORD': 'Abod@2000',
+'HOST': 'localhost',
+'PORT': 5432,
+}
 }
 
 
