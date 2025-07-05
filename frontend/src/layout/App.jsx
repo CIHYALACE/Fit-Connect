3041b58f36204008ae5307bdbce5a22a40db4855
AbdelRahman <abodyoussef2015@gmail.com>
2025-07-05 12:58:54 +0300

    Refactor AccountPage structure; replace Auth page with new Account page implementation

--- frontend/src/layout/App.jsx
+++ frontend/src/layout/App.jsx
@@ -2,7 +2,7 @@ import { Route, Router, Routes } from 'react-router-dom'
 import '../style/App.css'
 import SharedLayout from '../sharedLayout/sharedLayout'
 import Home from '../pages/Home'
-import AccountPage from '../pages/Auth'
+import AccountPage from '../pages/Account'
 
 export default function App() {
 
