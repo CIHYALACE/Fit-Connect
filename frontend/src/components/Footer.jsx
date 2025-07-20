f0445b1b9f9be31a62d7f219de3a2abdf2330476
AbdelRahman <abodyoussef2015@gmail.com>
2025-07-20 22:28:06 +0300

    Improve Footer style

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,161 +1,123 @@
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  const companyName = "FitConnect";
+  const companyDesc = "Your ultimate fitness companion connecting you with certified trainers and personalized workout plans.";
+  const links = [
+    { title: "Find Trainers", href: "/trainers" },
+    { title: "Training Programs", href: "/programs" },
+    { title: "About Us", href: "/about" },
+    { title: "Contact Us", href: "/contact" },
+  ];
+  const socialLinks = [
+    { icon: "fab fa-facebook", href: "#" },
+    { icon: "fab fa-instagram", href: "#" },
+    { icon: "fab fa-twitter", href: "#" },
+    { icon: "fab fa-youtube", href: "#" },
+  ];
+
+  const contactInfo = {
+    address: "123 Fitness Street, Gym City, 10001",
+    email: "abodyoussef2015@gmail.com",
+    phone: "+20 1027983379",
+    workingHours: "Mon-Fri: 9:00 AM - 8:00 PM\nSat-Sun: 10:00 AM - 6:00 PM"
+  };
+
+  
+
   return (
-    <>
-      <div className="mt-5">
-        <footer
-          className="text-center text-lg-start text-white bg-dark">
-          <section
-            className="d-flex justify-content-between p-4"
-            // style={{ backgroundColor: "#c10005" }}
-          >
-            <div className="me-5">
-              <span>Get connected with us on social networks:</span>
-            </div>
-
-            <div>
-              <a href="https://www.facebook.com/cihyalace.abdo/" target="_blank" className="text-white me-4">
-                <i className="fab fa-facebook-f"></i>
-              </a>
-              <a href="#" className="text-white me-4">
-                <i className="fa-brands fa-x-twitter"></i>
-              </a>
-              <a href="mailto:abodyoussef2015@gmail.com" className="text-white me-4">
-                <i className="fab fa-google"></i>
-              </a>
-              <a href="https://www.instagram.com/abdoyoussef_2/" target="_blank" className="text-white me-4">
-                <i className="fab fa-instagram"></i>
-              </a>
-              <a href="https://www.linkedin.com/in/abdel-rahman-youssef-16664b236/" target="_blank" className="text-white me-4">
-                <i className="fab fa-linkedin"></i>
-              </a>
-              <a href="https://github.com/CIHYALACE" target="_blank" className="text-white me-4">
-                <i className="fab fa-github"></i>
-              </a>
-            </div>
-          </section>
-
+    <div className="container-fluid p-0 mt-5">
+      <footer className="text-center text-lg-start text-white bg-gradient-dark">
+        <div className="container p-4 pb-0">
           <section className="">
-            <div className="container text-center text-md-start mt-5">
-              <div className="row mt-3">
-                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-                  <h6 className="text-uppercase fw-bold">Fit Connect</h6>
-                  <hr
-                    className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{
-                      width: "60px",
-                      backgroundColor: "#f40006",
-                      height: "2px",
-                    }}
-                  />
-                  <p>
-                    Here you can use rows and columns to organize your footer
-                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit.
-                  </p>
-                </div>
+            <div className="row g-4">
+              {/* Company Info */}
+              <div className="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">
+                  {companyName}
+                </h6>
+                <p className="opacity-85">
+                  {companyDesc}
+                </p>
+              </div>
 
-                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-                  <h6 className="text-uppercase fw-bold">Products</h6>
-                  <hr
-                    className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{
-                      width: "60px",
-                      backgroundColor: "#f40006",
-                      height: "2px",
-                    }}
-                  />
-                  <p>
-                    <a href="#!" className="text-white">
-                      Online Couching
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-white">
-                      Training Program
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-white">
-                      Meal Plans
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-white">
-                      Trainers
-                    </a>
-                  </p>
-                </div>
+              {/* Quick Links */}
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">Quick Links</h6>
+                <ul className="list-unstyled">
+                  {links.map((link, index) => (
+                    <li key={index} className="mb-2">
+                      <a 
+                        href={link.href} 
+                        className="text-white text-decoration-none d-inline-block py-1 px-2 rounded hover-bg"
+                      >
+                        {link.title}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
 
-                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-                  <h6 className="text-uppercase fw-bold">Useful links</h6>
-                  <hr
-                    className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{
-                      width: "60px",
-                      backgroundColor: "#f40006",
-                      height: "2px",
-                    }}
-                  />
-                  <p>
-                    <a href="#!" className="text-white">
-                      Home
+              {/* Contact Info */}
+              <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                <ul className="list-unstyled">
+                  <li className="mb-3 d-flex align-items-start">
+                    <i className="fas fa-map-marker-alt mt-1 me-3"></i>
+                    <span>{contactInfo.address}</span>
+                  </li>
+                  <li className="mb-3 d-flex align-items-start">
+                    <i className="fas fa-envelope mt-1 me-3"></i>
+                    <a href={`mailto:${contactInfo.email}`} className="text-white text-decoration-none">
+                      {contactInfo.email}
                     </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-white">
-                      About
+                  </li>
+                  <li className="mb-3 d-flex align-items-start">
+                    <i className="fas fa-phone-alt mt-1 me-3"></i>
+                    <a href={`tel:${contactInfo.phone.replace(/[^0-9+]/g, '')}`} className="text-white text-decoration-none">
+                      {contactInfo.phone}
                     </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-white">
-                      Sponsers
-                    </a>
-                  </p>
-                  <p>
-                    <a href="#!" className="text-white">
-                      Contact
-                    </a>
-                  </p>
-                </div>
+                  </li>
+                  <li className="mb-3 d-flex align-items-start">
+                    <i className="far fa-clock mt-1 me-3"></i>
+                    <span style={{whiteSpace: 'pre-line'}}>{contactInfo.workingHours}</span>
+                  </li>
+                </ul>
+              </div>
 
-                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                  <h6 className="text-uppercase fw-bold">Contact</h6>
-                  <hr
-                    className="mb-4 mt-0 d-inline-block mx-auto"
-                    style={{
-                      width: "60px",
-                      backgroundColor: "#f40006",
-                      height: "2px",
-                    }}
-                  />
-                  <p>
-                    <i className="fas fa-home mr-3"></i> New Cairo, 10012, Egypt
-                  </p>
-                  <p>
-                    <i className="fas fa-envelope mr-3"></i> abodyoussef2015@gmail.com
-                  </p>
-                  <p>
-                    <i className="fas fa-phone mr-3"></i> +020 10 279 833 79
-                  </p>
-                  <p>
-                    <i className="fas fa-print mr-3"></i> +020 10 279 833 79
-                  </p>
+              {/* Social Links */}
+              <div className="col-md-4 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h6 className="text-uppercase mb-4 font-weight-bold">Follow Us</h6>
+                <div className="d-flex flex-wrap justify-content-center justify-content-lg-start gap-2">
+                  {socialLinks.map((social, index) => (
+                    <a
+                      key={index}
+                      className="btn btn-floating m-0 rounded-circle d-flex align-items-center justify-content-center"
+                      style={{
+                        width: '40px',
+                        height: '40px',
+                        backgroundColor: index === 0 ? '#3b5998' : 
+                                        index === 1 ? '#E1306C' : 
+                                        index === 2 ? '#1DA1F2' : '#FF0000'
+                      }}
+                      href={social.href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      role="button"
+                    >
+                      <i className={social.icon}></i>
+                    </a>
+                  ))}
                 </div>
               </div>
             </div>
           </section>
+        </div>
 
-          <div
-            className="text-center p-3"
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-          >
-            © 2020 Copyright:
-            <a className="text-white" href="#">
-              Fit Connect
-            </a>
-          </div>
-        </footer>
-      </div>
-    </>
+        {/* Copyright */}
+        <div className="text-center p-3 border-top border-dark">
+          &copy; {currentYear} {companyName}. All rights reserved.
+        </div>
+      </footer>
+    </div>
   );
 }
